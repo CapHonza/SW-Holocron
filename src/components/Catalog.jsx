63b7ff81fb92c6a9930bdeeb4a15a77afe9698c6
CapHonza <capjan.work@gmail.com>
2026-07-07 01:00:01 +0200

    Dokončení filtrace postav a stylů pro katalog

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
 import "./Catalog.css"
 
+// Definice stavů
 export default function Catalog() {
     const [error, setError] = useState(null)
     const [character, setCharacter] = useState([]);
+    const [activeCategory, setActiveCategory] = useState('všichni');
     const fetchUrl = "https://raw.githubusercontent.com/akabab/starwars-api/master/api/all.json"
-    
+
+    // Fetch dat
     useEffect(function() {
         async function fetchData() {
             try {
@@ -20,15 +23,50 @@ export default function Catalog() {
             }
         }
         fetchData();
-    }, []);
+    }, []); //* Pole závislostí [] -> když je prázdné, tak funkce na ničem nezávisí -> 
+            //* tzn. spustí se pouze jednou a to jen při prvním načtení stránky / komponenty, 
+            //* jinak by se data stahovala neustále při každém kliknutí na tlačítko
 
+            //* PS.: Když tam bude závislost, tak se spustí, když si změní jedna z proměnných (závislostí) uvnitř
+
+    //* postava = parametr funkce (vymýšlím si ho sám)
+    //* Vezme první záznam, pojmenuje si ho dosšasně postava, zkontroluje if/else a propustí dál, veme druhý záznam, pojmenuje ho postava a znovu
+    const filteredCharacters = character.filter((postava) => { 
+        if (activeCategory === 'všichni') {
+            return true;
+        } else if (activeCategory === 'jedi') {
+            return postava.affiliations?.includes("Jedi Order"); //* ? = Optional Chaining - některé postavy třeba vůbec nemají affiliations (zvířata např.)
+        } else if (activeCategory === 'sith') {
+            return postava.affiliations?.includes('Sith'); //* Pokud by tam ? nebyl, JS by se snažil udělat metodu include na něčem, co neexistuje a aplikace by spadla
+        } else if (activeCategory === 'droid') {
+            return postava.species?.includes('droid') //* Říká: zkus se podívat do species, jestli to ta postava vůbec má, pokud ne, 
+                                                        //* vrať false a dál se ani nesnaž nic hledat, aby to nespadlo
+        } else if (activeCategory === 'others') {
+            return !postava.affiliations?.includes('Jedi Order') && 
+                    !postava.affiliations?.includes('Sith') &&
+                    !postava.species?.includes('droid');
+        } else {
+            return false;
+        }
+    });
+
+    // Tělo komponenty
     return (
         <div className="body-wrapper">
             <h1>Toto je katalog postav</h1>
             {error && <p className="error-message">{error}</p>}
+            <div className="buttons">
+                <button onClick={() => setActiveCategory('všichni')} className={activeCategory === 'všichni' ? 'active' : ''}>Všichni</button>
+                <button onClick={() => setActiveCategory('jedi')} className={activeCategory === 'jedi' ? 'active' : ''}>Jediové</button>
+                <button onClick={() => setActiveCategory('sith')} className={activeCategory === 'sith' ? 'active' : ''}>Sithové</button>
+                <button onClick={() => setActiveCategory('droid')} className={activeCategory === 'droid' ? 'active' : ''}>Droidi</button>
+                <button onClick={() => setActiveCategory('others')} className={activeCategory === 'others' ? 'active' : ''}>Ostatní</button>
+            </div>
+
+            
 
             <div className="character-grid">
-                {character.map((postava) => {
+                {filteredCharacters.map((postava) => {
                     return (
                         <div key={postava.id} className="character-card">
                             <h2>Jméno: <span className="value">{postava.name}</span></h2>
@@ -37,7 +75,7 @@ export default function Catalog() {
                             <img src={postava.image} alt={postava.name} />
                         </div>
                     )
-                })};
+                })}
             </div>
         </div>
     )
