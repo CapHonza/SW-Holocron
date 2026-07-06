d4dac5e502992f65f65b19848e79396a52e2e584
CapHonza <capjan.work@gmail.com>
2026-07-06 04:07:41 +0200

    Stylování a úprava Katalogu

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import "./Catalog.css"
 
 export default function Catalog() {
     const [error, setError] = useState(null)
@@ -22,15 +23,17 @@ export default function Catalog() {
     }, []);
 
     return (
-        <div>
-            <h2>Toto je katalog postav</h2>
+        <div className="body-wrapper">
+            <h1>Toto je katalog postav</h1>
             {error && <p className="error-message">{error}</p>}
 
             <div className="character-grid">
                 {character.map((postava) => {
                     return (
                         <div key={postava.id} className="character-card">
-                            <h2>Jméno: {postava.name}</h2>
+                            <h2>Jméno: <span className="value">{postava.name}</span></h2>
+                            <h2>Druh: <span className="value">{postava.species}</span></h2>
+                            <h2>Domovina: <span className="value">{postava.homeworld}</span></h2>
                             <img src={postava.image} alt={postava.name} />
                         </div>
                     )
